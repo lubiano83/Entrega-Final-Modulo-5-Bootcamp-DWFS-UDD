7ba3c360a26133c3ed9c3e8abc46c6ead9ef854a
lubiano83 <josepablolubiano@MacBook-Air-de-Jose.local>
2025-03-29 18:20:30 -0300

    ajustando hook

--- src/hook/useApi.hook.jsx
+++ src/hook/useApi.hook.jsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { ApiContext } from "../context/ApiContext";
 
-export function useApi() {
-  return useContext(ApiContext);
-}
+const useApi = () => {
+    return useContext(ApiContext);
+};
+
+export default useApi;

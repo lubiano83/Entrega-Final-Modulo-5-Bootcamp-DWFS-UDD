af533d71f068e04ae9d02eef128a7934856b4b7a
lubiano83 <josepablolubiano@MacBook-Air-de-Jose.local>
2025-03-29 20:11:51 -0300

    ajustes varios

--- src/component/Logo.jsx
+++ src/component/Logo.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Logo = () => {
   return (
     <Link href={"/"}>
-      <div className='text-lg font-bold text-white'>My Weather Spots</div>
+      <div className='text-lg font-bold text-white'>myWeatherSpots</div>
     </Link>
   )
 }

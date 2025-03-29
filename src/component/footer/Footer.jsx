51cd5f367a8e6f401e80823338846b55e9ebb1c1
lubiano83 <josepablolubiano@MacBook-Air-de-Jose.local>
2025-03-29 20:59:02 -0300

    ajustes varios

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = ({ email, derechos }) => {
 
 
   return (
-    <div className={`flex justify-evenly items-center gap-4 p-2 text-white ${isDarkMode ? "bg-amber-500" : "bg-blue-500" }`}>
+    <div className={`flex justify-evenly items-center gap-4 px-8 py-4 flex-wrap-reverse text-center text-white ${isDarkMode ? "bg-amber-500" : "bg-blue-500" }`}>
       {derechos}
       <a href="mailto:lubiano83@gmail.com" class="hover:text-gray-700">
         <h3>{email}</h3>

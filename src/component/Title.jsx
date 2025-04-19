0a38322cb64a5c0f3ed7710ffa21fa8f76534ffd
lubiano83 <josepablolubiano@lubiano83.local>
2025-04-19 18:51:06 -0400

    sacando italic al title

--- src/component/Title.jsx
+++ src/component/Title.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = ({ children, isDarkMode }) => {
 
   return (
-    <h1 className={`${isDarkMode ? "text-amber-700" : "text-green-700"} font-bold italic text-2xl`}>
+    <h1 className={`${isDarkMode ? "text-amber-700" : "text-green-700"} font-bold text-2xl`}>
         { children }
     </h1>
   )

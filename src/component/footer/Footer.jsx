c1c72d798e42874adf9246cde63c305c4d86ed41
lubiano83 <josepablolubiano@MacBook-Air-de-Jose.local>
2025-05-05 12:05:44 -0400

    ajustando contacto footer

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -6,9 +6,7 @@ const Footer = ({ email, derechos, isDarkMode }) => {
   return (
     <div className={`flex justify-evenly items-center gap-4 px-8 py-4 flex-wrap-reverse text-center text-white ${isDarkMode ? "bg-amber-700" : "bg-green-700" }`}>
       {derechos}
-      <a href="mailto:lubiano83@gmail.com" className="hover:text-gray-700">
-        <h3>Contacto: {email}</h3>
-      </a>
+      <h3>Contacto:<a href={`mailto:${email}`} className="hover:text-gray-700"> {email}</a></h3>
       <Logo />
     </div>
   )

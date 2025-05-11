0fce81d4f5ba73ca2130e81e9c55061d41843fbd
lubiano83 <josepablolubiano@lubiano83-2.local>
2025-05-11 16:37:21 -0400

    ajustes varios

--- src/component/NotFound.jsx
+++ src/component/NotFound.jsx
@@ -1,11 +1,18 @@
 import Message from "./Message";
 import GoBack from "./GoBack";
+import ErrorMessage from "./ErrorMessage";
 
 export default function NotFound({ isDarkMode }) {
-    return (
-        <div className="flex flex-col justify-center items-center gap-4">
-            <Message isDarkMode={isDarkMode}>¡Not Found: Error 404!</Message>
-            <GoBack isDarkMode={isDarkMode} />
-        </div>
-    )
+
+    try {
+        return (
+            <div className="flex flex-col justify-center items-center gap-4">
+                <Message isDarkMode={isDarkMode}>¡Not Found: Error 404!</Message>
+                <GoBack isDarkMode={isDarkMode} />
+            </div>
+        )
+    } catch (error) {
+        console.log(error.message);
+        return <ErrorMessage isDarkMode={isDarkMode} />
+    }
 };
